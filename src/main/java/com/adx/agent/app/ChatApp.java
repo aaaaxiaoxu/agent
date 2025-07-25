b804715dced53fc56a0b51edea431f6d5fffe281
aaaaxiaoxu <Liwei.Xu22@student.xjtlu.edu.cn>
2025-07-25 18:15:49 +0800

    second commit

--- src/main/java/com/adx/agent/app/ChatApp.java
+++ src/main/java/com/adx/agent/app/ChatApp.java
@@ -1,4 +1,52 @@
 package com.adx.agent.app;
 
+import com.adx.agent.advisor.MyLoggerAdvisor;
+import com.adx.agent.advisor.ReReadingAdvisor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
+
+@Component
+@Slf4j
 public class ChatApp {
+
+    private final ChatClient chatClient;
+
+    private static final String SYSTEM_PROMPT = "你是一个一人公司小管家，主要负责帮助你完成公司内的各种事务。";
+    public ChatApp(ChatModel dashscopeChatModel) {
+        // 初始化基于内存的对话记忆
+        ChatMemory chatMemory = new InMemoryChatMemory();
+        chatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new MyLoggerAdvisor()
+//                        new ReReadingAdvisor()
+                )
+                .build();
+    }
+
+    public String doChat(String message, String chatId) {
+        ChatResponse response = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .chatResponse();
+        String content = response.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
+
+
 }
--- src/test/java/com/adx/agent/app/ChatAppTest.java
+++ src/test/java/com/adx/agent/app/ChatAppTest.java
@@ -1,4 +1,34 @@
+package com.adx.agent.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest()
 class ChatAppTest {
-  
+
+    @Resource
+    private ChatApp chatApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员鱼皮";
+        String answer = chatApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮
+        message = "我想让另一半（编程导航）更爱我";
+        answer = chatApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = chatApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
 }
